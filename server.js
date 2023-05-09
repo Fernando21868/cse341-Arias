2975881cf074d8fe13846d39f16f3e9cd521214a
FERNANDO <elderarias2015@gmail.com>
2023-05-08 23:57:35 -0300

    Swaggerbug

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    "Access-Control-Allow-Origin",
+    "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
   );
   res.setHeader("Content-Type", "application/json");
